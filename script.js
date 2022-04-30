46dbed19a1f4623e1fa0e34bd35d502a97fdd3cc
Chiamaka Mary-Anne Ugwu <mannehills@gmail.com>
2022-04-30 22:13:09 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,50 +1,41 @@
-// var nav = header.getElementsByClassName("nav");
-
-// for (var i = 0; i < navElement.length; i++){
-//     nav[i].addEventListener("click", function(){
-//         var current = document.getElementsByClassName("active");
-//         current[i].className = current[i].className.replace(" active", "");
-//         this.className += " active";
-//     });
-// }
-
-
-// $(document).ready(function(){
-
-//     $('.nav').click(function(){
-//         $(".nav.active").removeClass("active");
-//         $(this).addClass('active');
-//     });
-// })
-
-
-
 const nav = document.getElementById("nav");
 const menuIcon = document.getElementById("menu-icon");
-
 const dropDown = document.getElementById("drop-down");
 const close = document.getElementById("close");
+const navs = document.querySelectorAll(".nav");
+const button = document.getElementById("button");
 
 
+// onscroll, navbar effects, scroll button
 window.onscroll = function(){
     var top = window.scrollY;
     if(top >= 50){
         nav.style.backgroundColor = "white";
         nav.style.filter = "drop-shadow(0px 3px 3px rgba(83, 83, 83, 0.15))";
+        button.style.display = "block";
+
     }
     else{
         nav.style.backgroundColor = "#f8f8f8";
         nav.style.filter = "none";
+        button.style.display = "none";
+
     }
 }
 
+// button function
+button.addEventListener("click", function(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
 
-    
 
+
+// hamburger menu
 menuIcon.addEventListener("click", function(){
-        close.style.display = "block";
-        menuIcon.style.display = "none";
-        dropDown.style.right = "0";
+    close.style.display = "block";
+    menuIcon.style.display = "none";
+    dropDown.style.right = "0";
 })
 
 close.addEventListener("click", function(){
@@ -55,6 +46,41 @@ close.addEventListener("click", function(){
 
 
 
+// nav links on 1000px max-width
+const x = window.matchMedia("(max-width: 1000px)");
+
+if(x.matches){
+    for(var i = 0; i < navs.length; i++){
+        navs[i].addEventListener('click', function(){
+            close.style.display = "none";
+            menuIcon.style.display = "block";
+            dropDown.style.right = "-100%";
+        })
+    }
+}
+
+
+
+// open and close for "see more"
+const text = document.querySelectorAll(".text");
+const more = document.querySelectorAll(".more")
+
+text.forEach((text) => {
+    text.addEventListener('click', () =>{
+        text.classList.toggle("active");
+    })
+})
+more.forEach((more) => {
+    more.addEventListener('click', () =>{
+        more.classList.toggle("active");
+    })
+})
+
+
+
+
 
 
 AOS.init();
+
+
